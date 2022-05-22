be01c8dbce7ed6b37d5ff1affb3fbe58ecde5567
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.ed.eg>
2022-05-22 20:40:32 +0200

    Clean code

--- main.py
+++ main.py
@@ -3,26 +3,23 @@ import hashlib
 import pandas as pd
 
 
+# Skip odd rows
 def logic(index):
     if index % 2 != 0:
         return True
     return False
 
 
-# Test case
-# data = pd.read_csv("testcase.csv", skiprows=lambda x: logic(x))
-
-# Actual Data
+# Read CSV file
 data = pd.read_csv("annual-enterprise-survey-2020-financial-year-provisional-csv.csv", skiprows=lambda x: logic(x))
 
 
-# print(data.Number)
-# print(data.Industry_code_NZSIOC)
-
+# concate result in one line
 result = ""
 for row in data.Industry_code_NZSIOC:
     result += str(row)
 
+# hash result
 hashed_object = hashlib.md5(result.encode())
-# print(result)
+
 print(hashed_object.hexdigest())
